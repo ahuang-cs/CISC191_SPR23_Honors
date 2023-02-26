35aa3b1d544c4597ab1135a33b16be9a0e767065
Alan S <rubixmaster821@gmail.com>
2023-02-25 16:11:46 -0800

    Added default constructor and setName() for MenuItem class. Added further documentation for each method.

--- Client/src/main/java/edu/sdccd/cisc191/template/MenuItem.java
+++ Client/src/main/java/edu/sdccd/cisc191/template/MenuItem.java
@@ -1,15 +1,19 @@
 package edu.sdccd.cisc191.template;
 
 /*
-    The MenuItem class will be an abstract class representing all menu items.
+    The MenuItem class will be an abstract class representing all menu items. Currently in progress.
 
-    MenuItem objects should contain:
+    All MenuItem objects should contain:
         - Item Name (String)
         - Item Recipe (Array of Ingredients)
 
     Subclass Ideas:
         - Drink sizes (affects ingredient requirements)
         - Flavors / Variants
+        - Create Boolean for hot and cold drinks
+
+    Abstract Classes Ideas
+        -
 
     TODO:   Create functional MenuItem Class
             Add subclasses of MenuItem
@@ -20,19 +24,45 @@ public class MenuItem
 {
     private String itemName;        // Name of this menuItem (Cannot be changed later)
 
-    // Constructor
+    public MenuItem()
+    {
+        itemName = "";
+    }
+
+    /**
+     * This is the constructor for MenuItem objects.
+     * @param name The name of the Menu Item.
+     */
     public MenuItem(String name)
     {
         itemName = name;
     }
 
+    /**
+     * This method returns the name of a menu item.
+     * @return String representing name of menu item.
+     */
     public String getName()
     {
         return itemName;
     }
 
+    /**
+     * This method sets the name of a menu item
+     * @param name The new name of the menu item.
+     */
+    public void setName(String name)
+    {
+        itemName = name;
+    }
+
+    /**
+     * This method prints out a string representing this menuItem object.
+     * @return The Menu Item's name.
+     */
     public String toString()
     {
+        // TODO: Print all relevant information (Ingredients, etc.).
         return itemName;
     }
 
