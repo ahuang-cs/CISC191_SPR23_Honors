fed12a8fb971ac45abe6613f0ff22425dd398687
mdamgen0 <mdamgen@student.sdccd.edu>
2023-02-27 07:11:39 -0800

    Developed Default Constructor and Constructor for the Pastry Class Understand Warnings and will address in the future

--- Client/src/main/java/edu/sdccd/cisc191/template/Pastry.java
+++ Client/src/main/java/edu/sdccd/cisc191/template/Pastry.java
@@ -11,6 +11,28 @@ package edu.sdccd.cisc191.template;
 
 public class Pastry extends MenuItem {
 
+    private String icingFlavor;
+    private boolean hasGluten;
 
+    //Default Constructor
+    public Pastry()
+    {
+        super();
+        this.icingFlavor = "Chocolate";
+        this.hasGluten = true;
+    }
+    /**
+     * Constructor
+     * @param name The name of the pastry
+     * @param hasGluten Whether the pastry is gluten free (false) or not (true)
+     * @param icingFlavor Determines what flavor icing the pastry will have.
+     */
+    public Pastry(String name, double salePrice,String icingFlavor, boolean hasGluten)
+    {
+        super(name, salePrice);
+        this.hasGluten = hasGluten;
+        this.icingFlavor = icingFlavor;
+
+    }
 
 }
