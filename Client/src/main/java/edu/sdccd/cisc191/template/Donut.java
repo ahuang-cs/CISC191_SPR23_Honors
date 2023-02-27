c6dcf76d820169dd29d62528eda7de10ff6124fb
mdamgen0 <mdamgen@student.sdccd.edu>
2023-02-27 07:39:45 -0800

    Generated a comment for ideas for different possible types of donuts Understand Warnings and will address in the future

--- Client/src/main/java/edu/sdccd/cisc191/template/Donut.java
+++ Client/src/main/java/edu/sdccd/cisc191/template/Donut.java
@@ -1,5 +1,16 @@
 package edu.sdccd.cisc191.template;
 
+/*
+    Donuts
+        -Double Chocolate
+        -Chocolate Frosted
+        -Strawberry Frosted
+        -Vanilla Frosted
+        -Glazed
+        -Boston Creme
+        -Jelly
+
+ */
 public class Donut extends Pastry{
 
     public boolean hasSprinkles;        //Whether the donut has sprinkles or not
