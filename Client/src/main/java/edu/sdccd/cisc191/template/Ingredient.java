a8250fd7ce7cec0db3138a9b8b9ba871872927c0
Alan S <rubixmaster821@gmail.com>
2023-03-01 18:09:43 -0800

    Added class fields to Ingredient.java, subject to change.

--- Client/src/main/java/edu/sdccd/cisc191/template/Ingredient.java
+++ Client/src/main/java/edu/sdccd/cisc191/template/Ingredient.java
@@ -1,4 +1,20 @@
 package edu.sdccd.cisc191.template;
 
-public class Ingredient {
+public class Ingredient
+{
+    // ********** CLASS FIELDS **********
+    private String name;                // The name of the ingredient
+    private double pricePerUnit;        // The unit price of the ingredient
+    private String expirationDate;      // The date at which this ingredient expires. (Perhaps change to shelf life?)
+    private boolean isLiquid;           // True if ingredient is liquid, false if solid / powder
+    private boolean isOrganic;          // True if ingredient is organic, false otherwise
+    private boolean hasCaffeine;        // True if ingredient has caffeine, false otherwise
+    private boolean hasGluten;          // True if ingredient has gluten, false otherwise
+    private boolean veganFriendly;      // True if ingredient has no animal products, false if otherwise
+
+    // ********** END OF CLASS FIELDS **********
+
+
+
+
 }
