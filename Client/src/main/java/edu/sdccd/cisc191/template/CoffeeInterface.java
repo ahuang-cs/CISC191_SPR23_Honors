c9e2f300872ba577ebfcf20b192777713cd1968a
Jay Lee <jiahuilee92117@gmail.com>
2023-03-09 11:50:02 -0800

    interface for the coffee class

---
+++ Client/src/main/java/edu/sdccd/cisc191/template/CoffeeInterface.java
@@ -0,0 +1,33 @@
+package edu.sdccd.cisc191.template;
+
+public interface CoffeeInterface {
+    // this implements the coffee class and defines several methods related to making different types of coffee.
+    void makeCoffee();
+    void makeLatte(String espressoType, String milkType, boolean hasFoam, String flavor);
+    void makeCappuccino(int espresso, int steamedMilk, int foam, String flavor);
+    void makeFrozen(boolean isBlended, String flavor);
+    void makeIced(boolean hasMilk);
+}
+
+public class coffee extends Drink implements CoffeeInterface {
+
+    public void makeCoffee() {
+        // implementation of our coffee recipe
+    }
+
+    public void makeLatte(String espressoType, String milkType, boolean hasFoam, String flavor) {
+        // uses our Latte recipe
+    }
+
+    public void makeCappuccino(int espresso, int steamedMilk, int foam, String flavor) {
+        // uses our cappuccino recipe
+    }
+
+    public void makeFrozen(boolean isBlended, String flavor) {
+        // uses our frozen coffee recipe
+    }
+
+    public void makeIced(boolean hasMilk) {
+        // uses our iced coffee recipe
+    }
+}
