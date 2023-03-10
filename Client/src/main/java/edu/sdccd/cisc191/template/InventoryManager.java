4d11221c37ec42f7af7ce596a10be0a3553436fd
Alan S <rubixmaster821@gmail.com>
2023-03-09 20:50:53 -0800

    Created arrays for managing ingredients, menu items, and recipes.

--- Client/src/main/java/edu/sdccd/cisc191/template/InventoryManager.java
+++ Client/src/main/java/edu/sdccd/cisc191/template/InventoryManager.java
@@ -1,14 +1,14 @@
 package edu.sdccd.cisc191.template;
 
-/**
+/*
  * This class stores 2D arrays containing MenuItems, Ingredients, and their respective amounts.
  * This class also contains a recipe book which maps MenuItems and Ingredients.
  *
  * Fields:
  *  - String[] ingredientList;      <-- Holds a list of ingredients (strings for now)
  *  - MenuItems[] menuItemList;     <-- Holds a list of all menu items
- *  - int[][2] ingredientInventory; <-- Maps items from the ingredientList to the amount in stock
- *  - int[][2] menuItemInventory;   <-- Maps items from the menuItemList to the amount in stock
+ *  - int[][] ingredientInventory;  <-- Maps items from the ingredientList to the amount in stock
+ *  - int[][] menuItemInventory;    <-- Maps items from the menuItemList to the amount in stock
  *  - int[][] recipeBook;           <-- Maps items from the ingredientList to each item on the menuItem list
  *
  * Required Methods:
@@ -32,14 +32,42 @@ package edu.sdccd.cisc191.template;
  *  + void addToRecipe(String itemName, String ingredient, int amount); <-- Adds an ingredient to a menuItem's recipe
  *  + void removeFromRecipe(String itemName, String ingredient);        <-- Removes an ingredient from menuItem recipe
  * ********** END OF RECIPE SPECIFIC METHODS **********
+ *
+ * ********** OTHER METHODS **********
+ *  - void increaseIngredientListCapacity();
+ *  - void increase menuItemListCapacity();
+ *  - void increaseIngredientInventoryCapacity();
+ *  - void increaseMenuItemInventoryCapacity();
+ *  - void increaseRecipeBookItemCapacity();
+ *  - void increaseRecipeBookIngredientCapacity();
+ * ********** END OF OTHER METHODS ***********
  */
 public class InventoryManager
 {
     // ************************ DATA ***********************
 
+    // Holds a list of all ingredients as strings.
+    private String[] ingredientList;
 
+    // Holds a list of all menu items.
+    private MenuItem[] menuItemList;
+
+    // Maps all ingredients in ingredientList to the amount that is in storage.
+    //      - Column 0 is ingredient index, column 1 is amount of that ingredient.
+    private int[][] ingredientInventory;
+
+    // Maps all MenuItems in MenuItemList to the amount that is available.
+    //      - Column 0 is the menuItem index, column 1 is the amount of that menu item available.
+    private int[][] menuItemInventory;
+
+    // Maps all MenuItems in MenuItemList to a set of ingredients in IngredientList.
+    //      - Column 0 is the menuItem, odd and even columns alternate between ingredient index and amount.
+    private int[][] recipeBook;
 
     // ******************** END OF DATA ********************
+
+
+
 
 
 }
