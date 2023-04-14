1deadf9d6f3ddd1caa9b447eb2de780c1836ef99
Alan S <rubixmaster821@gmail.com>
2023-04-14 09:53:59 -0700

    Added MenuNode.java

---
+++ Server/src/main/java/edu/sdccd/cisc191/template/MenuNode.java
@@ -0,0 +1,20 @@
+package edu.sdccd.cisc191.template;
+
+import edu.sdccd.cisc191.template.MenuItem.MenuItem;
+
+public class MenuNode
+{
+    // ********** Variable and Object Creation ***********
+    protected MenuItem item;        // The item stored in the inventory
+    protected MenuNode nextNode;    // The node after this one in the list.
+    // ******* End of Variable and Object Creation *******
+
+    /**
+     * Constructor for MenuNode objects.
+     * @param addedItem     The Menu Item that this node represents.
+     */
+    public MenuNode(MenuItem addedItem)
+    {
+        item  = addedItem;
+    }
+}
