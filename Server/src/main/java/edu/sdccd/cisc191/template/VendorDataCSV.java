3df929324c1db68dfeae446966ee5060e1b38ad9
faqua <enguyen014@student.sdssc.edu>
2023-04-22 16:24:52 -0700

    - change to two method: write and read a file

--- Server/src/main/java/edu/sdccd/cisc191/template/VendorDataCSV.java
+++ Server/src/main/java/edu/sdccd/cisc191/template/VendorDataCSV.java
@@ -2,6 +2,7 @@ package edu.sdccd.cisc191.template;
 
 //import com.opencsv.CSVReader;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.sdccd.cisc191.template.Ingredient.Ingredient;
@@ -14,13 +15,10 @@ import java.util.List;
 public class VendorDataCSV implements VendorData {
     private Vendor vendor;
 
-    public VendorDataCSV() {
+    public VendorDataCSV()throws IOException {
         //vendor = newVendor;
     }
-
-    @Override
-    public List<CostcoCSV> importCostcoVendor() throws URISyntaxException, IOException {
-
+    public void writeToFile() throws IOException {
         /** convert a list/JAVA into JSON format **/
         ObjectMapper mapper = new ObjectMapper();
         List<CostcoCSV> list = new ArrayList<>();
@@ -49,6 +47,43 @@ public class VendorDataCSV implements VendorData {
 
         CostcoCSV costco8 = new CostcoCSV("Flour", Ingredient.Units.LB, 1.99);
         list.add(costco8);
+
+        //convert these ingredient data into Jason format and write to a new file
+        String jsonData = mapper.writeValueAsString(list); //convert our Object into Jason data
+        mapper.writeValue(new File("Costco.json"), list); //write to a Jason file
+
+    }
+    @Override
+    public List<CostcoCSV> importCostcoVendor() throws URISyntaxException, IOException {
+
+        /** convert a list/JAVA into JSON format **/
+        ObjectMapper mapper = new ObjectMapper();
+        List<CostcoCSV> list = new ArrayList<>();
+
+//        //Add the ingredients to the list
+//        CostcoCSV costco1 = new CostcoCSV("Creamer", Ingredient.Units.GAL, 5.99);
+//        list.add(costco1);
+//
+//        CostcoCSV costco2 = new CostcoCSV("Coffee Beans", Ingredient.Units.LB, 14.99);
+//        list.add(costco2);
+//
+//        CostcoCSV costco3 = new CostcoCSV("Sugar", Ingredient.Units.LB, 29.99);
+//        list.add(costco3);
+//
+//        CostcoCSV costco4 = new CostcoCSV("Milk", Ingredient.Units.GAL, 5.99);
+//        list.add(costco4);
+//
+//        CostcoCSV costco5 = new CostcoCSV("Yeast", Ingredient.Units.LB, 4.99);
+//        list.add(costco5);
+//
+//        CostcoCSV costco6 = new CostcoCSV("Eggs", Ingredient.Units.NUM, 0.99);
+//        list.add(costco6);
+//
+//        CostcoCSV costco7 = new CostcoCSV("Butter", Ingredient.Units.LB, 4.99);
+//        list.add(costco7);
+//
+//        CostcoCSV costco8 = new CostcoCSV("Flour", Ingredient.Units.LB, 1.99);
+//        list.add(costco8);
 
         //convert these ingredient data into Jason format and write to a new file
         String jsonData = mapper.writeValueAsString(list); //convert our Object into Jason data
